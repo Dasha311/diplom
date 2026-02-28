79afd92eee76d9fee6953f0c089900608405c34e
Dasha311 <dbezrukova482@gmail.com>
2026-02-28 20:22:23 +0500

    update project

--- InfoPost/university/urls.py
+++ InfoPost/university/urls.py
@@ -7,6 +7,14 @@ app_name = 'university'
 urlpatterns = [
     path('', views.main_menu, name='main_menu'),
     path('schools/', views.schools_menu, name='schools_menu'),
+    path('schools/management/', views.school_of_management, name='school_of_management'),
+    path('schools/economics-and-finance/', views.school_of_economics, name='school_of_economics'),
+    path('schools/politics-and-law/', views.school_of_politics, name='school_of_politics'),
+    path('schools/media-and-cinema/', views.school_of_media, name='school_of_media'),
+    path('schools/entrepreneurship/', views.school_of_business, name='school_of_business'),
+    path('schools/hospitality-and-tourism/', views.school_of_tourism, name='school_of_tourism'),
+    path('schools/health-sciences/', views.sharmanov_school, name='sharmanov_school'),
+    path('schools/transformative-humanities/', views.school_of_transformative, name='school_of_transformative'),
     path('schools/digital-technologies/', views.school_of_digital, name='school_of_digital'),
     path('programs/information-systems/', views.info_systems_menu, name='info_systems_menu'),
 ]
--- InfoPost/university/views.py
+++ InfoPost/university/views.py
@@ -11,6 +11,37 @@ def schools_menu(request):
 def school_of_digital(request):
     return render(request, 'SchoolOfDigitalMenus.html')
 
+def school_of_management(request):
+    return render(request, 'SchoolOfManagement.html')
+
+
+def school_of_economics(request):
+    return render(request, 'SchoolOfEconomics.html')
+
+
+def school_of_politics(request):
+    return render(request, 'SchoolOfPolitics.html')
+
+
+def school_of_media(request):
+    return render(request, 'SchoolOfMedia.html')
+
+
+def school_of_business(request):
+    return render(request, 'SchoolOfBusiness.html')
+
+
+def school_of_tourism(request):
+    return render(request, 'SchoolOfTourism.html')
+
+
+def sharmanov_school(request):
+    return render(request, 'SharmanovSchool.html')
+
+
+def school_of_transformative(request):
+    return render(request, 'School ofTransformative.html')
+
 
 def info_systems_menu(request):
     return render(request, 'InfoSystemsMenu.html')
